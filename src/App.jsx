a272c5ac82e13b03a000538617c6dd51a50831db
Itoh Shimon <shimon@postfixnotation.org>
2025-02-10 23:43:57 +0900

    結果画面のモックアップ

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 //import Setup from './Setup.jsx'
-import Compare from './Compare.jsx'
-//import Result from './Result.jsx'
+//import Compare from './Compare.jsx'
+import Result from './Result.jsx'
 
 export default function App() {
   return (
     <>
-      <Compare sorterTitle="すき" />
+      {/*<Setup />*/}
+      {/*<Compare sorterTitle="すき" />*/}
+      <Result sorterTitle="すき" chars={Array(40).fill('キャラ').map((c, i) => `${c}${i+1}`)} />
       <footer className="footer">開発: <a href="https://postfixnotation.org/" target="_blank">志文</a></footer>
     </>
   )
--- src/Result.jsx
+++ src/Result.jsx
@@ -1,17 +1,26 @@
-export default function Result() {
-  return (
-    <>
-      <h1>連縁キャラソート</h1>
-      <h2>...ランキング</h2>
-      {Array(10).fill(null).map((_, i) =>
-        <div>{i+1}位 ...</div>
-      )}
-      <p>(〜分、〜回)</p>
-      <div>Xで共有</div>
-      <div>リンク</div>
-      <button>もう一度</button>
+import { Fragment } from 'react'
+import './Result.css'
 
-      <div>開発: <a href="https://postfixnotation.org/">志文</a></div>
-    </>
+export default function Result({ sorterTitle, chars }) {
+  return (
+    <div className="result">
+      <h1 className="result-title">連縁キャラソート</h1>
+      <h2 className="result-subtitle">{sorterTitle}ランキング</h2>
+      <div className="result-chars-container">
+        {
+          chars.slice(0, 10).map((c, i) =>
+            <Fragment key={i}>
+              <span className="result-chars-rank">{i+1}位</span>
+              <span>{c}</span>
+            </Fragment>
+          )
+        }
+      </div>
+      <div className="result-info">(〜分、〜回)</div>
+      <hr className="result-hr-main-sub" />
+      <a href="https://example.com/" target="_blank">Xで共有</a>
+      <a className="result-result-link" href="https://example.com/" target="_blank">結果へのリンク</a>
+      <button className="result-retry">もう一度</button>
+    </div>
   );
 }
