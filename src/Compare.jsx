c1897c8ed4a8d1459cc752948074cbbedc3b46f1
Itoh Shimon <shimon@postfixnotation.org>
2025-02-10 22:33:54 +0900

    比較画面のレスポンシブ対応

--- src/Compare.jsx
+++ src/Compare.jsx
@@ -4,15 +4,17 @@ export default function Compare({ sorterTitle }) {
   return (
     <div className="compare">
       <h1 className="compare-title">どっちが{sorterTitle}？</h1>
-      <button className="compare-char1">キャラ1</button>
-      <button className="compare-char2">あいうえおかきくけこさしすせそ</button>
-      <button className="compare-both">両方</button>
-      <button className="compare-skip">あとまわし</button>
+      <div className="compare-main">
+        <button className="compare-char1">キャラ1</button>
+        <button className="compare-char2">あいうえおかきくけこさしすせそ</button>
+        <button className="compare-both">両方</button>
+        <button className="compare-skip">あとまわし</button>
+      </div>
       <hr className="compare-hr-main-sub" />
       <button className="compare-undo">↶</button>
       <button className="compare-redo">↷</button>
       <div className="compare-info">(n組目、n%完了、残り最大n組)</div>
-      <button className="compare-quit">ソートをやめる</button>
+      <button className="compare-quit">キャラソートをやめる</button>
     </div>
   );
 }
