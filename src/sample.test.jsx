007233c9489895c7b2443185331a523bd9a4fe32
Itoh Shimon <shimon@postfixnotation.org>
2025-02-13 00:23:39 +0900

    テストランナーをJestからVitestに変更。
    JestはViteを十分サポートしておらず、CSSのインポートなどで躓く。
    そこで、Viteと相性が良く、しかもモダンJS環境をよくサポートするVitestを
    代わりに使用する。

--- src/sample.test.jsx
+++ src/sample.test.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/vitest';
 
 test('チェックボックスをクリックして有効にする', async () => {
   const user = userEvent.setup();
