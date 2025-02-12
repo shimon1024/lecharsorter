d94d8f9b120bf49b92fb6b2f243fc79bc1b3409e
Itoh Shimon <shimon@postfixnotation.org>
2025-02-12 22:29:36 +0900

    テスト環境を導入

---
+++ src/sample.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+test('チェックボックスをクリックして有効にする', async () => {
+  const user = userEvent.setup();
+  render(<input type="checkbox" />);
+
+  expect(screen.getByRole('checkbox')).not.toBeChecked();
+
+  await user.click(screen.getByRole('checkbox'));
+
+  expect(screen.getByRole('checkbox')).toBeChecked();
+});
