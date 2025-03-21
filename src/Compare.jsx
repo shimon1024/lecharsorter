fd83146cbd47fa29180c5148ab8f33fc49950b14
Itoh Shimon <shimon@postfixnotation.org>
2025-03-22 02:14:51 +0900

    比較の進捗率の表示を実装。

--- src/Compare.jsx
+++ src/Compare.jsx
@@ -5,6 +5,31 @@ import { SceneSetContext } from './SceneContext.jsx';
 import * as le from './lenen.js'
 import * as sorter from './sorter.js';
 
+function calcProgress(sortHistory, numRanks) {
+  // ゴールがヒープ木が空になるかランキングが必要分集まるかの2種類あって、どちらかを達成すればいいと考える。
+  // 2種類のゴールそれぞれについて進捗を計算し、より進みが大きい方を返す。
+  //
+  // 例えば、ランキングの制限が緩い(全キャラソートかつ全キャラ選出など)条件で、heapifyを終えた後にextractで
+  // ずっとマージし続けた場合、ランキングの観点で見ると進捗が無いが、ヒープ木の観点で見るとノードが減り続けるため、
+  // 全体では進捗があると言える。
+  //
+  // 逆に、ランキングの制限が厳しい(全キャラソートかつ3位までなど)条件で、heapifyを終えた後にextractでマージせずに
+  // 選択を続けた場合、ヒープ木の観点では進捗が少ないが、ランキングの観点で見るとランキング制限に対して順調にノードが増えるため、
+  // 全体では進捗があると言える。
+
+  const denoHeapify = sortHistory.steps[0].start;
+  const denoExtractNumNodes = sortHistory.steps[0].heaptree.length;
+  const denoExtractNumRanks = numRanks;
+  const numeHeapify = denoHeapify - sortHistory.steps[sortHistory.currentStep].start;
+  const numeExtractNumNodes = denoExtractNumNodes - sortHistory.steps[sortHistory.currentStep].heaptree.length;
+  const numeExtractNumRanks = sortHistory.steps[sortHistory.currentStep].ranking.length;
+
+  return Math.max(
+    (numeHeapify + numeExtractNumNodes) / (denoHeapify + denoExtractNumNodes),
+    (numeHeapify + numeExtractNumRanks) / (denoHeapify + denoExtractNumRanks),
+  );
+}
+
 export default function Compare({ charIdSet, numRanks, sorterTitle, randSeed }) {
   const [sortHistory, setSortHistory] = useState(() => sorter.newSortHistory({ charIdSet, numRanks, randSeed }));
   const setScene = useContext(SceneSetContext);
@@ -12,13 +37,14 @@ export default function Compare({ charIdSet, numRanks, sorterTitle, randSeed }) 
   const { heaptree, ai, bi, aj, bj, sortState, ranking } = sortHistory.steps[sortHistory.currentStep];
   const aId = heaptree[ai][aj];
   const bId = heaptree[bi][bj];
+  const progressPercent = Math.floor(calcProgress(sortHistory, numRanks) * 100);
 
   function handleClick(action) {
     const newSortHistory = sorter.reduceSortHistory(sortHistory, action);
     setSortHistory(newSortHistory);
     const newStep = newSortHistory.steps[newSortHistory.currentStep];
     // TODO 自動保存。IndexedDB?redo後の選択に伴う履歴の廃棄に対応することを忘れずに。
-    // TODO n%完了、残り最大n組->n%完了だけでいいかも
+
     if (newStep.sortState === 'end') {
       setScene(
         <Result
@@ -66,7 +92,7 @@ export default function Compare({ charIdSet, numRanks, sorterTitle, randSeed }) 
       >
         ↷
       </button>
-      <div className="compare-info">({sortHistory.steps.length}組目、n%完了、残り最大n組)</div>
+      <div className="compare-info">({sortHistory.currentStep + 1}組目、{progressPercent}%完了)</div>
       <button className="compare-quit">キャラソートをやめる</button>
     </div>
   );
